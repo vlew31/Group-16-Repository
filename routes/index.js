77ef2522bd451196b525e50aa0c88ab0cbbf543f
fullsunn22 <102833294+fullsunn22@users.noreply.github.com>
2024-04-18 23:48:03 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,10 @@
+import clothesRoutes from './clothes.js';
 
+const constructorMethod = (app) => {
+    app.use('/', clothesRoutes);
+    app.use('*', (req, res) => {
+        res.status(404).json({ error: "Not Found" });
+    });
+};
+
+export default constructorMethod;
