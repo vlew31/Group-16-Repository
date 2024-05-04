297166398a6f19645136d0325afca8b29590df5b
fullsunn <jan@stevens.edu>
2024-05-04 18:38:32 -0400

    login info update

--- data/users.js
+++ data/users.js
@@ -1,229 +1,290 @@
 
-// import { users } from '../config/mongoCollections.js';
-// import {ObjectId} from 'mongodb';
-// import bcrypt from 'bcrypt';
-// const saltRounds = 2;
-
-// export const registerUser = async (
-//   firstName,
-//   lastName,
-//   email,
-//   username,
-//   password,
-//   role
-// ) => {
-//   if(firstName === undefined || lastName === undefined || email === undefined || username === undefined || password === undefined || role === undefined) {
-//       throw "All fields need to be supplied";
-//   }
-
-//   const fn = firstName.trim();
-//   const ln = lastName.trim();
-//   const e = email.trim();
-//   const u = username.trim().toLowerCase();
-//   const p = password.trim();
-//   const r = role.toLowerCase().trim();
-
-//   // const letterChecker = /^[a-zA-Z]+$/;
-//   // if(typeof fn !== 'string' || fn.length < 2 || fn.length > 25 || !letterChecker.test(fn)) {
-//   //   throw "invalid first name input";
-//   // }
-
-//   // if(typeof ln !== 'string' || ln.length < 2 || ln.length > 25 || !letterChecker.test(ln)) {
-//   //   throw "invalid last name input";
-//   // }
-
-//   // const emailChecker = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-//   // if(typeof e !== 'string' || emailChecker.test(e) === false) {
-//   //   throw "invalid email input";
-//   // }
-
-//   // if(typeof u !== 'string' || u.length < 5 || ln.length > 10 || !letterChecker.test(u)) {
-//   //   throw "invalid username input";
-//   // }
-
-//   // const upper = /[A-Z]/;
-//   // const number = /[0-9]/;
-//   // const specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
-//   // if(typeof p !== 'string' || p.length < 8 || !upper.test(p) || !number.test(p) || !specialChar.test(p)) {
-//   //   throw "invalid password input from registerUser";
-//   // }
-
-
-//   // if(r !== 'user' && r !== 'admin') {
-//   //   throw "invalid role input";
-//   // }
-
-//   const userList = await users();
-
-//   let h = await bcrypt.hash(password, saltRounds);
-//   let newUser = {
-//     firstName: fn,
-//     lastName: ln,
-//     email: e,
-//     username: u,
-//     password: h,
-//     role: r
-//   }
-
-//   let insertedUser = await userList.insertOne(newUser);
-//   if (!insertedUser.insertedCount === 0) {
-//     throw "username taken. try again";
-//   }
-
-//   newUser._id =  newUser._id.toString();
-
-//   return { signupCompleted: true };
-//   return newUser;
-
-// };
-
-// export const loginUser = async (username, password) => {
-
-//   const u = username.trim();
-//   const p = password.trim();
-
-//   if(u === undefined || p === undefined) {
-//     throw "both inputs must be supplied";
-//   }
-
-//   const letterChecker = /^[a-zA-Z]+$/;
-//   if(typeof u !== 'string' || u.length < 5 || u.length > 10 || !letterChecker.test(u)) {
-//     throw "invalid username input";
-//   }
-
-//   const upper = /[A-Z]/;
-//   const number = /[0-9]/;
-//   const specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
-//   if(typeof p !== 'string' || p.length < 8 || !upper.test(p) || !number.test(p) || !specialChar.test(p)) {
-//     throw "invalid password input from loginUser";
-//   }
-
-//   const userList = await users();
-//   const findUser = await userList.findOne({ username: u });
-
-//   if (!findUser) {
-//     throw "Either the username or password is invalid";
-//   }
-
-//   if (await bcrypt.compare(password, findUser.password) == false) {
-//     throw "Either the username or password is invalid";
-//   }
-//   return {
-//     firstName: findUser.firstName, 
-//     lastName: findUser.lastName, 
-//     email: findUser.email,
-//     username: findUser.username,  
-//     role: findUser.role
-//   };
-// };
-  
   //import mongo collections, bcrypt and implement the following data functions
   import { users } from "../config/mongoCollections.js";
   import bcrypt from "bcryptjs";
-  export const registerUser = async (
-	  firstName,
-	  lastName,
-	  email,
-	  username,
-	  password,
-	  role
-	) => {
-	  firstName = firstName.trim();
-	  lastName = lastName.trim();
-	email = email.trim();
-	  username = username.trim();
-	  password = password.trim();
-	  role = role.trim();
+//   export const registerUser = async (
+// 	  firstName,
+// 	  lastName,
+// 	  email,
+// 	  username,
+// 	  password,
+// 	  role
+// 	) => {
+// 	  firstName = firstName.trim();
+// 	  lastName = lastName.trim();
+// 	  email = email.trim();
+// 	  username = username.trim();
+// 	  password = password.trim();
+// 	  role = role.trim();
 	  
-	  const lowercaseUsername = username.toLowerCase();
-	  const usersCollection = await users();
-	  const existingUser = await usersCollection.findOne({
-		  username: lowercaseUsername,
-	  });
-	  if (existingUser) {
-		  throw "ERR: user already exists";
-	  }
+// 	  const lowercaseUsername = username.toLowerCase();
+// 	  const usersCollection = await users();
+// 	  const existingUser = await usersCollection.findOne({
+// 		  username: lowercaseUsername,
+// 	  });
+// 	  if (existingUser) {
+// 		  throw "ERR: user already exists";
+// 	  }
   
-	  const lowercaseRole = role.toLowerCase();
-	  if (lowercaseRole !== "admin" && lowercaseRole !== "user") {
-		  throw "ERR: Invalid role";
-	  }
+// 	  const lowercaseRole = role.toLowerCase();
+// 	  if (lowercaseRole !== "admin" && lowercaseRole !== "user") {
+// 		  throw "ERR: Invalid role";
+// 	  }
   
-	  //TODO! change 2 to 16 or smthing
-	  const hashedPassword = await bcrypt.hash(password, 2);
-	  const newUser = {
-		  firstName: firstName.trim(),
-		  lastName: lastName.trim(),
-	  email: email.trim(),
-		  username: lowercaseUsername,
-		  password: hashedPassword,
-		  role: lowercaseRole,
-	  };
-	  const insertedUser = await usersCollection.insertOne(newUser);
+// 	  //TODO! change 2 to 16 or smthing
+// 	  const hashedPassword = await bcrypt.hash(password, 2);
+// 	  const newUser = {
+// 		  firstName: firstName.trim(),
+// 		  lastName: lastName.trim(),
+// 	  email: email.trim(),
+// 		  username: lowercaseUsername,
+// 		  password: hashedPassword,
+// 		  role: lowercaseRole,
+// 	  };
+// 	  const insertedUser = await usersCollection.insertOne(newUser);
   
-	  if (insertedUser.insertedId) {
-		  return { signupCompleted: true };
-	  } else {
-		  throw "ERR: Failed to register user";
-	  }
-  };
+// 	  if (insertedUser.insertedId) {
+// 		  return { signupCompleted: true };
+// 	  } else {
+// 		  throw "ERR: Failed to register user";
+// 	  }
+//   };
   
-  export const loginUser = async (username, password) => {
-	  username = username.trim();
-	  password = password.trim();
+//   export const loginUser = async (username, password) => {
+// 	  username = username.trim();
+// 	  password = password.trim();
   
-	  // checkRequiredFields(username, password);
+// 	  // checkRequiredFields(username, password);
   
-	  // //username
-	  // checkIsProperLength(username, 5);
-	  // checkMaxLength(username, 10);
-	  // checkIfContainsNumber(username);
-	  const lowercaseUsername = username.toLowerCase();
+// 	  // //username
+// 	  // checkIsProperLength(username, 5);
+// 	  // checkMaxLength(username, 10);
+// 	  // checkIfContainsNumber(username);
+// 	  const lowercaseUsername = username.toLowerCase();
   
-	  //pasword
-	  // checkIsProperLength(password, 8);
-	  // if (
-	  // 	/\s/.test(password) ||
-	  // 	!/[A-Z]/.test(password) ||
-	  // 	!/\d/.test(password) ||
-	  // 	!/[!@#$%^&*(),.?":{}|<>]/.test(password)
-	  // ) {
-	  // 	throw "ERR: invalid password";
-	  // }
+// 	  //pasword
+// 	  // checkIsProperLength(password, 8);
+// 	  // if (
+// 	  // 	/\s/.test(password) ||
+// 	  // 	!/[A-Z]/.test(password) ||
+// 	  // 	!/\d/.test(password) ||
+// 	  // 	!/[!@#$%^&*(),.?":{}|<>]/.test(password)
+// 	  // ) {
+// 	  // 	throw "ERR: invalid password";
+// 	  // }
   
-	  const usersCollection = await users();
-	  const user = usersCollection.findOne({
-		  username: lowercaseUsername,
-	  });
-	  if (!user) {
-		  throw "Either the username or password is invalid";
-	  }
-	  const passwordMatch = bcrypt.compare(password, password);
-	  if (!passwordMatch) {
-		  throw "Either the username or password is invalid";
-	  }
+// 	  const usersCollection = await users();
+// 	  const user = usersCollection.findOne({
+// 		  username: lowercaseUsername,
+// 	  });
+// 	  if (!user) {
+// 		  throw "Either the username or password is invalid";
+// 	  }
+// 	  const passwordMatch = bcrypt.compare(password, password);
+// 	  if (!passwordMatch) {
+// 		  throw "Either the username or password is invalid";
+// 	  }
   
-	  const newUser = await usersCollection.findOne(
-		  { username: username },
-		  { projection: { password: 0 } }
-	  );
-	  const {
-		  firstName,
-		  lastName,
-	  email,
-		  username: storedUsername,
-		  role
-	  } = newUser;
+// 	  const newUser = await usersCollection.findOne(
+// 		  { username: username },
+// 		  { projection: { password: 0 } }
+// 	  );
+// 	  const {
+// 		  firstName,
+// 		  lastName,
+// 	  email,
+// 		  username: storedUsername,
+// 		  role
+// 	  } = newUser;
   
-	  return {
-		  firstName,
-		  lastName,
-	  email,
-		  username: storedUsername,
-		  role,
-	  };
-  };
+// 	  return {
+// 		  firstName,
+// 		  lastName,
+// 	  email,
+// 		  username: storedUsername,
+// 		  role,
+// 	  };
+//   };
 
+export const registerUser = async (
+    firstName,
+    lastName,
+    email,
+    username,
+    password,
+    role,
+	listings
+  ) => {
+	firstName = firstName.trim();
+	lastName = lastName.trim();
+  	email = email.trim();
+	username = username.trim();
+	password = password.trim();
+	role = role.trim();
+	// checkRequiredFields(
+	// 	firstName,
+	// 	lastName,
+	// 	username,
+	// 	password,
+	// 	favoriteQuote,
+	// 	themePreference,
+	// 	role
+	// );
+	// checkIfFieldsAreProperString(
+	// 	firstName,
+	// 	lastName,
+	// 	username,
+	// 	password,
+	// 	favoriteQuote,
+	// 	themePreference,
+	// 	role
+	// );
+	// //first
+	// checkIsProperLength(firstName, 2);
+	// checkMaxLength(firstName, 25);
+	// checkIfContainsNumber(firstName);
+	// //last
+	// checkIsProperLength(lastName, 2);
+	// checkMaxLength(lastName, 25);
+	// checkIfContainsNumber(lastName);
+	// //user
+	// checkIsProperLength(username, 5);
+	// checkMaxLength(username, 10);
+	// checkIfContainsNumber(username);
+
+	// console.log("here1");
+	//check duplicate
+	const lowercaseUsername = username.toLowerCase();
+	const usersCollection = await users();
+	const existingUser = await usersCollection.findOne({
+		username: lowercaseUsername,
+	});
+	if (existingUser) {
+		throw "ERR: user already exists";
+	}
+	// console.log("here2");
+
+	//pw
+	// checkIsProperLength(password, 8);
+	// if (
+	// 	/\s/.test(password) ||
+	// 	!/[A-Z]/.test(password) ||
+	// 	!/\d/.test(password) ||
+	// 	!/[!@#$%^&*(),.?":{}|<>]/.test(password)
+	// ) {
+	// 	throw "ERR: invalid password";
+	// }
+
+	//quote
+	// checkIsProperLength(favoriteQuote, 20);
+	// checkMaxLength(favoriteQuote, 255);
+
+	//theme
+	// const lowercaseThemePreference = themePreference.toLowerCase();
+	// if (
+	// 	lowercaseThemePreference !== "light" &&
+	// 	lowercaseThemePreference !== "dark"
+	// ) {
+	// 	throw "ERR: Invalid themePreference";
+	// }
+
+	//role
+	const lowercaseRole = role.toLowerCase();
+	if (lowercaseRole !== "admin" && lowercaseRole !== "user") {
+		throw "ERR: Invalid role";
+	}
+
+	//TODO! change 2 to 16 or smthing
+	const hashedPassword = await bcrypt.hash(password, 2);
+	const newUser = {
+		firstName: firstName.trim(),
+		lastName: lastName.trim(),
+    	email: email.trim(),
+		username: lowercaseUsername,
+		password: hashedPassword,
+		role: lowercaseRole,
+		listings: []
+	};
+	const insertedUser = await usersCollection.insertOne(newUser);
+
+	if (insertedUser.insertedId) {
+		return { signupCompleted: true };
+	} else {
+		throw "ERR: Failed to register user";
+	}
+};
+
+export const loginUser = async (username, password) => {
+	username = username.trim();
+	password = password.trim();
+
+	// checkRequiredFields(username, password);
+
+	// //username
+	// checkIsProperLength(username, 5);
+	// checkMaxLength(username, 10);
+	// checkIfContainsNumber(username);
+	const lowercaseUsername = username.toLowerCase();
+
+	//pasword
+	// checkIsProperLength(password, 8);
+	// if (
+	// 	/\s/.test(password) ||
+	// 	!/[A-Z]/.test(password) ||
+	// 	!/\d/.test(password) ||
+	// 	!/[!@#$%^&*(),.?":{}|<>]/.test(password)
+	// ) {
+	// 	throw "ERR: invalid password";
+	// }
+
+	const usersCollection = await users();
+	const user = usersCollection.findOne({
+		username: lowercaseUsername,
+	});
+	if (!user) {
+		throw "Either the username or password is invalid";
+	}
+	const passwordMatch = bcrypt.compare(password, password);
+	if (!passwordMatch) {
+		throw "Either the username or password is invalid";
+	}
+
+	const newUser = await usersCollection.findOne(
+		{ username: username },
+		{ projection: { password: 0 } }
+	);
+	const {
+		firstName,
+		lastName,
+   	 	email,
+		username: storedUsername,
+		role,
+		listings
+	} = newUser;
+
+	return {
+		firstName,
+		lastName,
+ 		email,
+		username: storedUsername,
+		role,
+		listings
+	};
+};
+
+
+  export const getAll = async () => {
+
+	const userCollection = await users();
+	let userList = await userCollection.find({}).project({_id: 1, username: 1}).toArray();
+  
+	if(!userList){
+	  throw 'users not found';
+	}
+  
+	return userList;
+  };
+  
   export const get = async (userId) => {
 	if(userId === undefined){
 	  throw 'id not provided';
@@ -239,20 +300,53 @@
 	}
   
 	userToFind._id = userToFind._id.toString();
-	
+  
 	return userToFind;
   };
   
+  export const remove = async (userId) => {
+	  if(userId === undefined){
+		throw 'id not provided';
+	  } else if(typeof userId !== 'string' || userId.trim().length === 0){
+		throw 'parameter is not a valid id';
+	  } else if(!ObjectId.isValid(userId.trim())){
+		throw 'id is not a valid ObjectID'
+	  } 
+	  const userCollection = await users();
+	  const deletedUser = await userCollection.findOneAndDelete({
+		_id: new ObjectId(userId)});
+	
+	  if (!deletedUser) {
+		throw `Could not delete the product with id ${id}`;
+	  }
+	  return `${deletedUser.username} has been successfully deleted!`;
+	};
   
-  export const getAll = async () => {
+   export const update = async (
+	  firstName,
+	  lastName,
+	  email,
+	  username,
+	  password,
+	  role
+	) => {
   
-	const userCollection = await users();
-	let userList = await userCollection.find({}).project({_id: 1, userame: 1}).toArray();
+	  let updatedUser = {
+		  _id: _id,
+		  firstName: firstName,
+		  lastName: lastName,
+		  email: email,
+		  username: username,
+		  password: password,
+		  role: role
+		  };
   
-	if(!userList){
-	  throw 'prducts not found';
-	}
+	  const userCollection = await users();
+	  const updated = await userCollection.updateOne({_id: new ObjectId(_id)},  {$set: updatedUser});
+	  if (!updated){
+		throw 'could not update';
+	  }
+	  const updatedUserWithId = Object.assign({ _id: _id }, updatedUser);
   
-	return userList;
-  };
-  
+	  return updatedUserWithId;
+	};
--- routes/users.js
+++ routes/users.js
@@ -28,95 +28,188 @@ router.route('/search').get(async (req, res) => {
   res.render('search', { title: 'Search Results' });
 });
 
+// router
+//   .route('/users/register')
+//   .get(async (req, res) => {
+//     return res.render("register", {title: "Register Page"});
+//   })
+//   .post(async (req, res) => {
+//     try {
+//       const {firstName, lastName, email, username, password, role} = req.body;
+//       if(firstName === undefined) {
+//         throw {code: 400, error: "first name is missing"};
+//       }
+//       if(lastName === undefined) {
+//         throw {code: 400, error: "last name is missing"};
+//       }
+//       if(username === undefined) {
+//         throw {code: 400, error: "username is missing"};
+//       }
+//       if(password === undefined) {
+//         throw {code: 400, error: "password is missing"};
+//       }
+//       // if(confirmPassword === undefined) {
+//       //   throw {code: 400, error: "confirm password is missing"};
+//       // }
+//       if(email === undefined) {
+//         throw {code: 400, error: "email is missing"};
+//       }
+//       if(role === undefined) {
+//         throw {code: 400, error: "role is missing"};
+//       }
+
+//       const letterChecker = /^[a-zA-Z]+$/;
+//       if(firstName.length < 2 || firstName.length > 25) {
+//         throw {code: 400, error: "first name must be between 2 to 25 characters long"};
+//       }
+
+//       if(!letterChecker.test(firstName)) {
+//         throw {code: 400, error: "first name must only contain letters"};
+//       }
+//       if(lastName.length < 2 || lastName.length > 25) {
+//         throw {code: 400, error: "last name must be between 2 to 25 characters long"};
+//       }
+//       if(username.length < 5 || username.length > 10) {
+//         throw {code: 400, error: "username must be between 5 to 10 characters long"};
+//       }
+
+//       if(!letterChecker.test(username)) {
+//         throw {code: 400, error: "username must only contain letters"};
+//       }
+
+//       const upper = /[A-Z]/;
+//       const number = /[0-9]/;
+//       const specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+
+//       if(password.length < 8) {
+//         throw {code: 400, error: "password must be at least 8 characters long"};
+//       }
+//       if(!upper.test(password) || !number.test(password) || !specialChar.test(password)) {
+//         if(!upper.test(password)) {
+//           throw {code: 400, error: "password must contain at least one uppercase letter"};
+//         }
+
+//         if(!number.test(password)) {
+//           throw {code: 400, error: "password must contain at least one number"};
+//         }
+
+//         if(!specialChar.test(password)) {
+//           throw {code: 400, error: "password must contain at least one special character"};
+//         }
+
+//       }
+
+//       if(role !== 'user' && role !== 'admin') {
+//         throw {code: 400, error: "invalid role input"};
+//       }
+//       const user = await registerUser(firstName, lastName, email, username, password, role);
+//       if (user.signupCompleted) {
+//         return res.redirect('/login');
+//       }
+//       else {
+//         return res.status(500).render('error: Could not register user');
+//       }
+//     }
+//     catch (e) {
+//       if (e.code) {
+//         return res.status(e.code).render('register', { errors: true, error: e.error})
+//       }
+//       return res.status(400).render('error');
+//     }
+//   });
+
 router
   .route('/users/register')
   .get(async (req, res) => {
-    return res.render("register", {title: "Register Page"});
+    return res.render("register", { title: "Register Page" });
   })
   .post(async (req, res) => {
     try {
-      const {firstName, lastName, email, username, password, role} = req.body;
-      if(firstName === undefined) {
-        throw {code: 400, error: "first name is missing"};
-      }
-      if(lastName === undefined) {
-        throw {code: 400, error: "last name is missing"};
-      }
-      if(username === undefined) {
-        throw {code: 400, error: "username is missing"};
-      }
-      if(password === undefined) {
-        throw {code: 400, error: "password is missing"};
-      }
-      // if(confirmPassword === undefined) {
-      //   throw {code: 400, error: "confirm password is missing"};
-      // }
-      if(email === undefined) {
-        throw {code: 400, error: "email is missing"};
-      }
-      if(role === undefined) {
-        throw {code: 400, error: "role is missing"};
+      const { firstName, lastName, email, username, password, role } = req.body;
+      console.log(req.body);
+      // Validate input data
+      if (!firstName || !lastName || !email || !username || !password || !role) {
+        throw { code: 400, error: "All fields are required" };
       }
 
-      const letterChecker = /^[a-zA-Z]+$/;
-      if(firstName.length < 2 || firstName.length > 25) {
-        throw {code: 400, error: "first name must be between 2 to 25 characters long"};
+      // Check for username uniqueness in the database
+      const existingUser = await usersData.getUserByUsername(username);
+      if (existingUser) {
+        throw { code: 400, error: "Username already exists" };
       }
 
-      if(!letterChecker.test(firstName)) {
-        throw {code: 400, error: "first name must only contain letters"};
-      }
-      if(lastName.length < 2 || lastName.length > 25) {
-        throw {code: 400, error: "last name must be between 2 to 25 characters long"};
-      }
-      if(username.length < 5 || username.length > 10) {
-        throw {code: 400, error: "username must be between 5 to 10 characters long"};
-      }
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-      if(!letterChecker.test(username)) {
-        throw {code: 400, error: "username must only contain letters"};
-      }
+      // Create new user in the database
+      const newUser = await usersData.registerUser({
+        firstName,
+        lastName,
+        email,
+        username,
+        password: hashedPassword,
+        role,
+      });
 
-      const upper = /[A-Z]/;
-      const number = /[0-9]/;
-      const specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+      // Set session data
+      req.session.user = {
+        id: newUser._id,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        email: newUser.email,
+        role: newUser.role,
+      };
 
-      if(password.length < 8) {
-        throw {code: 400, error: "password must be at least 8 characters long"};
+      // Redirect to profile page after successful registration
+      return res.redirect('/user');
+    } catch (error) {
+      // Handle registration errors
+      if (error.code) {
+        return res.status(error.code).render('register', { errors: true, error: error.error });
       }
-      if(!upper.test(password) || !number.test(password) || !specialChar.test(password)) {
-        if(!upper.test(password)) {
-          throw {code: 400, error: "password must contain at least one uppercase letter"};
-        }
-
-        if(!number.test(password)) {
-          throw {code: 400, error: "password must contain at least one number"};
-        }
-
-        if(!specialChar.test(password)) {
-          throw {code: 400, error: "password must contain at least one special character"};
-        }
-
-      }
-
-      if(role !== 'user' && role !== 'admin') {
-        throw {code: 400, error: "invalid role input"};
-      }
-      const user = await registerUser(firstName, lastName, email, username, password, role);
-      if (user.signupCompleted) {
-        return res.redirect('/login');
-      }
-      else {
-        return res.status(500).render('error: Could not register user');
-      }
-    }
-    catch (e) {
-      if (e.code) {
-        return res.status(e.code).render('register', { errors: true, error: e.error})
-      }
-      return res.status(400).render('error');
+      return res.status(500).render('error', { error: "Internal Server Error" });
     }
   });
+
+router
+  .route('/users/login')
+  .get(async (req, res) => {
+    return res.render("login", { title: "Login Page" });
+  })
+  .post(async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      // Validate input data
+      if (!username || !password) {
+        throw { code: 400, error: "Username and password are required" };
+      }
+
+      // Authenticate user
+      const user = await usersData.authenticateUser(username, password);
+      if (!user) {
+        throw { code: 401, error: "Invalid username or password" };
+      }
+
+      // Set session data
+      req.session.user = {
+        id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        role: user.role,
+      };
+
+      // Redirect to profile page after successful login
+      return res.redirect('/users/profile');
+    } catch (error) {
+      // Handle login errors
+      if (error.code) {
+        return res.status(error.code).render('login', { errors: true, error: error.error });
+      }
+      return res.status(500).render('error', { error: "Internal Server Error" });
+    }
+  });
+
 
 router
   .route('/users/login')
@@ -189,7 +282,7 @@ router
       return res.status(500).render('error')
     }
   });
-router
+  router
   .route('/users')
   .get(async (req, res) => {
     console.log("hey");
@@ -197,7 +290,11 @@ router
       const users = await userData.getAll();
       const simplifiedUsers = users.map(user => ({
         _id: user._id,
-        username: user.username
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        username: user.username,
+        role: user.role
       }));
       res.json(simplifiedUsers);
     } catch (e) {
@@ -223,7 +320,7 @@ router
   });
 
 router
-  .route('/users/:userId')
+  .route('/users/:_id')
   .get(async (req, res) => {
     const userId = req.params._id;
     try {
@@ -238,13 +335,41 @@ router
         res.status(400).json({ error: e.message });
       }
     }
+  })
+  .delete(async (req, res) => {
+    try {
+      let deletedUser = await userData.remove(req.params._id);
+      res.status(200).json({ _id: deletedUser, deleted: true });
+    } catch (e) {
+      res.status(404).json({error: e});
+    }
+  })
+  .put(async (req, res) => {
+    try {
+      const updatedUser = await userData.update(
+        req.params._id,
+        req.body.firstName,
+        req.body.lastName,
+        req.body.email,
+        req.body.username,
+        req.body.password,
+        req.body.role,
+      );
+      res.status(200).json(updatedUser);
+    } catch (e) {
+      if(e === 'could not update'){
+        res.status(404).json({error: 'user not found'});
+      } else {
+        return res.status(400).send({error: e});
+      }
+    }
   });
 
   router.route('/admin').get(async (req, res) => {
     return res.render('admin', {title: "Admin Page", ...req.session.user, currentTime: new Date().toUTCString()});
   });
 
-  router.route('/logout').get(async (req, res) => {
+  router.route('/users/logout').get(async (req, res) => {
     req.session.destroy();
     res.clearCookie('AuthenticationState', '', { expires: new Date() });
     return res.render("logout", {title: "Logout Page"});
