6c9e2938455a8b5f960a8e65399160d4c829a8a5
ivillanu <ivillanu@stevens.edu>
2024-05-05 23:43:58 -0400

    added more to the seed

--- seed.js
+++ seed.js
@@ -21,6 +21,7 @@ async function testRegisterUser(){
 			"ivillanu@stevens.edu",
 			"ivillanu",
 			"ivillanu123",
+			"ivillanu123",
 			"admin"
 		);
 	} catch(e) {
@@ -33,6 +34,7 @@ async function testRegisterUser(){
 			"Ampaw",
 			"fake_email@gmail.com",
 			"Keba12345", 
+			"Keba1234",
 			"Keba1234",
 			"admin"
 		);
@@ -47,6 +49,7 @@ async function testRegisterUser(){
 			"super_fake_email@gmail.com",
 			"Belalipop", 
 			"YeatLover",
+			"YeatLover",
 			"admin"
 		);
 	} catch(e) {
@@ -59,6 +62,7 @@ async function testRegisterUser(){
 			"villanu",
 			"real_email@gmail.com",
 			"Missbelly", 
+			"BeaLover",
 			"BeaLover",
 			"user"
 		);
@@ -80,16 +84,16 @@ async function testCreateProducts() { //creates a listing
 	try {
 		fortniteMerch = await clothesData.create(
 			"      missbelly     ",
-			"fortnite merch",
+			"Fortnite Merch",
 			"limited edition fortnite nettspend collab",
 			"hoodie",
 			"XXS",
-			"blue",
+			"Blue",
 			"Unisex",
 			49.99,
 			"New",
 			["Nettspend", "Underground", "Fortnite", "Merch"],
-			["https://downersclub.com/wp-content/uploads/2023/11/Nettspend.jpg", "https://cdn1.epicgames.com/offer/fn/Blade_2560x1440_2560x1440-95718a8046a942675a0bc4d27560e2bb"]
+			["https://i.pinimg.com/564x/45/7a/9e/457a9e34639bf34422a0139fccf053d3.jpg", "https://cdn1.epicgames.com/offer/fn/Blade_2560x1440_2560x1440-95718a8046a942675a0bc4d27560e2bb"]
 			
 		);
 	} catch (e) {
@@ -99,16 +103,16 @@ async function testCreateProducts() { //creates a listing
 	try {
 		beaMerch = await clothesData.create(
 			"belalipop",
-			"beabadoobee merch",
+			"Beabadoobee Merch",
 			"I <3 BEABADOOBEE",
 			"jorts",
 			"XXL",
-			"black",
-			"F",
+			"Black",
+			"Female",
 			19.91,
 			"Refurbished",
 			["Beabadoobee", "Singer", "Filipina", "Denim"],
-			["https://i1.sndcdn.com/artworks-yi5PWVKO8kzMtSPc-tFa0hA-t240x240.jpg"]
+			["https://i.pinimg.com/564x/c3/86/20/c386207388e9fab69c7fcdf2dac84dca.jpg", "https://i1.sndcdn.com/artworks-yi5PWVKO8kzMtSPc-tFa0hA-t240x240.jpg"]
 		);
 	} catch (e) {
 		console.log("Uh-oh, not supposed to see me - 2");
@@ -117,11 +121,11 @@ async function testCreateProducts() { //creates a listing
 	try {
 		cartiMerch = await clothesData.create(
 			"keba123",
-			"hello kitty mug",
+			"Hello Kitty Mug",
 			"playboi carti mug from EvilJ0rdan music video!",
 			"accessories",
 			"N/A",
-			"white",
+			"White",
 			"Unisex",
 			20.24,
 			"Used",
@@ -157,8 +161,8 @@ async function testCreateProducts() { //creates a listing
 			"Backless denim vest with denim ribbon",
 			"Vest",
 			"M",
-			"blue",
-			"F",
+			"Blue",
+			"Male",
 			90.00,
 			"Refurbished",
 			["Denim", "Ribbon", "Coquette", "Backless"],
@@ -176,11 +180,29 @@ async function testCreateProducts() { //creates a listing
 			"Tee",
 			"S",
 			"Other",
-			"F",
+			"Female",
 			48.00,
 			"New",
 			["Mesh", "Moon", "Street Style"],
 			["https://i.pinimg.com/736x/67/16/85/671685f5580e935b903519e96e892c94.jpg", "https://i.pinimg.com/736x/db/e4/51/dbe451c0af54a2bd40b2437a58b68487.jpg"]
+		);
+	} catch (e) {
+		console.log("Uh-oh, not supposed to see me - 2");
+		console.log(e);
+	}
+	try {
+		moonTee = await clothesData.create(
+			"missbelly",
+			"Vintage Polo Ralph Lauren Equestrian Lined Denim Trucker Jacket",
+			"This jacket is really comfortable and it's good for providing warmth.",
+			"Jacket",
+			"L",
+			"Blue",
+			"Unisex",
+			199.99,
+			"Used",
+			["Equestrian", "Vintage", "Polo Ralph Lauren"],
+			["https://i.pinimg.com/736x/21/b2/14/21b21472c6fa7e602d2eb484f5d7a011.jpg", "https://i.pinimg.com/736x/50/ff/86/50ff86cbffc2d142b1d2464b858008e1.jpg", "https://i.pinimg.com/736x/26/90/f9/2690f9c611e5894de62bfec8bfb5f1e8.jpg"]
 		);
 	} catch (e) {
 		console.log("Uh-oh, not supposed to see me - 2");
